d73c2213a182fc48a3d074d24a417151bedd6533
zonaetmunna <zonaetmonna@gmail.com>
2021-09-13 02:14:12 +0600

    solve

--- js/app.js
+++ js/app.js
@@ -4,28 +4,55 @@ const loadProducts = () => {
     .then((response) => response.json())
     .then((data) => showProducts(data));
 };
-loadProducts();
+
+// loadProducts();
 
 // show all product in UI 
 const showProducts = (products) => {
+  // console.log(products);
   const allProducts = products.map((pd) => pd);
+  // console.log(allProducts);
   for (const product of allProducts) {
-    const image = product.images;
+    console.log(product);
+    const images = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
     div.innerHTML = `<div class="single-product">
       <div>
-    <img class="product-image" src=${image}></img>
+    <img class="product-image" src=${images}></img>
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="loadSingleProduct(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
 };
+// loadSingleProduct()
+const loadSingleProduct = (productId) => {
+  // console.log(productId)
+  const url = `https://fakestoreapi.com/products/${productId}`;
+  // console.log(url);
+  fetch(url)
+    .then(res => res.json())
+    .then(data => displaySinglePrduct(data))
+}
+
+const displaySinglePrduct = (data) => {
+  const singleResult = document.getElementById("single-result");
+  singleResult.innerHTML = '';
+  singleResult.innerHTML = `
+  <img src="${data.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">${data.title}</h5>
+        <h3>Price:$ ${data.price}</h3>
+        <p class="card-text">${data.description.slice(0, 50)}</p>
+      </div>
+  `
+}
+
 let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
@@ -37,18 +64,22 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
+
+// fixs place
 // main price update function
 const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
+
+// fix place
 // set innerText function
 const setInnerText = (id, value) => {
   document.getElementById(id).innerText = Math.round(value);
