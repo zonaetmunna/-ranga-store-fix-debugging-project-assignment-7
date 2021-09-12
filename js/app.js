24a93bc6117d782a6b360ae27ad7d7e2d7ea5fc2
zonaetmunna <zonaetmonna@gmail.com>
2021-09-13 02:51:35 +0600

    fullfi

--- js/app.js
+++ js/app.js
@@ -5,8 +5,6 @@ const loadProducts = () => {
     .then((data) => showProducts(data));
 };
 
-// loadProducts();
-
 // show all product in UI 
 const showProducts = (products) => {
   // console.log(products);
@@ -23,6 +21,8 @@ const showProducts = (products) => {
       </div>
       <h3>${product.title}</h3>
       <p>Category: ${product.category}</p>
+      <h5>Rating:${product.rating.rate}<h5>
+      <h5>Average Rating:${product.rating.count}<h5>
       <h2>Price: $ ${product.price}</h2>
       <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
       <button onclick="loadSingleProduct(${product.id})" id="details-btn" class="btn btn-danger">Details</button></div>
@@ -30,6 +30,8 @@ const showProducts = (products) => {
     document.getElementById("all-products").appendChild(div);
   }
 };
+
+
 // loadSingleProduct()
 const loadSingleProduct = (productId) => {
   // console.log(productId)
@@ -47,6 +49,8 @@ const displaySinglePrduct = (data) => {
   <img src="${data.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${data.title}</h5>
+        <h5>Rating:${data.rating.rate}<h5>
+        <h5>Average Rating:${data.rating.count}<h5>
         <h3>Price:$ ${data.price}</h3>
         <p class="card-text">${data.description.slice(0, 50)}</p>
       </div>
@@ -57,8 +61,8 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -78,11 +82,10 @@ const updatePrice = (id, value) => {
   document.getElementById(id).innerText = total.toFixed(2);
 };
 
-
 // fix place
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -107,5 +110,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  console.log(grandTotal);
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
